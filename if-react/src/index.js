adf3e8060fac69ae5508d7f4c72b984a4bccb817
parkjunyoung <hpaaycim1@naver.com>
2022-07-16 12:40:50 +0900

    '가 아니라 물결 ` 눌러야됨

--- if-react/src/index.js
+++ if-react/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import Library from './chapter_03/Library';
+
+// 만든 파일을 실제로 랜더링 하려면 index.js 파일을 수정해야됨 
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Library/>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

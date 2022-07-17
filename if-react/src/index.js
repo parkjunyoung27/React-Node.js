c0885e1db8f735c6bfd4d278283523d9c02444e4
parkjunyoung <hpaaycim1@naver.com>
2022-07-17 10:40:44 +0900

    Clock 함수

--- if-react/src/index.js
+++ if-react/src/index.js
@@ -5,15 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
 // 만든 파일을 실제로 랜더링 하려면 index.js 파일을 수정해야됨 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Library/>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+setInterval(()=> { 
+  ReactDOM.render(
+    <React.StrictMode>
+      <Clock/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+},1000); //1초마다 갱신 
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Library/>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

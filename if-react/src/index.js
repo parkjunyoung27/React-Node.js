13695c3ae90e92f98296cb9a0a5416321ebcf49d
parkjunyoung <hpaaycim1@naver.com>
2022-07-18 23:29:45 +0900

    styling

--- if-react/src/index.js
+++ if-react/src/index.js
@@ -15,14 +15,14 @@ import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Caculator';
 import ProfileCard from './chapter_13/ProfileCard';
-import DarkOrLight from './chapter_14/DarkOrLight';
+import DarkOrLight from './chapter_14/DarkOrLight.jsx';
+import Blocks from './chapter_15/Blocks';
 
 // 만든 파일을 실제로 랜더링 하려면 index.js 파일을 수정해야됨 
 
-//chapter_05
 ReactDOM.render(
   <React.StrictMode>
-    <ProfileCard/>    
+    <DarkOrLight/>    
   </React.StrictMode>,
   document.getElementById('root')
 );

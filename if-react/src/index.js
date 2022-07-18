9f085c2a8e4b5b8d4f427e121c5e1ac44e9028f8
parkjunyoung <hpaaycim1@naver.com>
2022-07-18 22:06:12 +0900

    Context

--- if-react/src/index.js
+++ if-react/src/index.js
@@ -14,7 +14,8 @@ import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Caculator';
-import ProfileCard from './chapter_13/ProfileCard'
+import ProfileCard from './chapter_13/ProfileCard';
+import DarkOrLight from './chapter_14/DarkOrLight';
 
 // 만든 파일을 실제로 랜더링 하려면 index.js 파일을 수정해야됨 
 

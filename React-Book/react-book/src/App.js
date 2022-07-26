123b847bacb913cb5363bea7893619702d72b31d
parkjunyoung <hpaaycim1@naver.com>
2022-07-27 00:01:35 +0900

    make_component

--- React-Book/react-book/src/App.js
+++ React-Book/react-book/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Counter from './Counter';
 import Say from './say';
+import EventPractice from './EventPractice';
 
 function App() {
   // return <MyComponent name="박준영"/>;
-  return <Say/>;
+  return <EventPractice/>;
 };
 
 export default App;
---
+++ React-Book/react-book/src/EventPractice.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+
+const EventPractice = () => {
+    const [form, setForm] = useState({ // 중요
+        username: '',
+        message: ''
+    });
+
+    const [ username, message ] = form;
+
+    const onChange = e => {
+        const nextForm = {
+            ...form, //기존의 form내용을 이 자리에 복사한 뒤
+            [e.target.name] : e.target.value //원하는 값을 덮어 씌우기 
+        };
+        setForm(nextForm);
+    };
+    
+    const onClick = () => {
+        alert(username + ': ' + message);
+        setForm({
+            username:'',
+            message: ''
+        });
+    };
+
+    const onKeyPress = e =>{
+        if(e.key === 'Enter'){
+            onClick();
+        }
+    };
+        return(
+            <div>
+                <h1>이벤트 연습</h1> 
+                <input
+                    type="text" name="username" placeholder="사용자명"
+                    value={username}
+                    // onChange={(e) => {console.log(e)}}
+                    onChange={onChange}
+                />
+                <input
+                    type="text" name="message" placeholder="아무거나 입력해 보세요"
+                    value={message}
+                    // onChange={(e) => {console.log(e)}}
+                    onChange={onChange}
+                    onKeyPress={onKeyPress}
+                />
+                
+                {/* <button onClick={
+                    () => {
+                        alert(this.state.message);
+                        this.setState({
+                            message: ''
+                        });
+                    }}>
+                        확인
+                </button> */}
+
+                <button onClick={this.handleClick}>확인</button>
+                
+            </div>
+        );
+};
+
+export default EventPractice;

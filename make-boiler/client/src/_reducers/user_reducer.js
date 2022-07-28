7e6e56f9a4ffd2e7d2aa9df650ff1798c385a24d
parkjunyoung <hpaaycim1@naver.com>
2022-07-29 00:14:32 +0900

    register2

--- make-boiler/client/src/_reducers/user_reducer.js
+++ make-boiler/client/src/_reducers/user_reducer.js
@@ -1,5 +1,6 @@
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from '../_actions/types';
 
 export default function (state = {}, action) {
@@ -7,6 +8,10 @@ export default function (state = {}, action) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
                  // ...는 빈상태로 가져오는 뜻
+            break;
+        case REGISTER_USER:
+            return { ...state, register: action.payload }
+                // ...는 빈상태로 가져오는 뜻
             break;
         default:
             return state;
--- make-boiler/client/src/components/views/RegisterPage/RegisterPage.js
+++ make-boiler/client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,46 +1,58 @@
 import React, { useState } from 'react'
-import Axios from 'axios'
 import { useDispatch } from 'react-redux'
-import { loginUser } from '../../../_actions/user_action'
+import { registerUser } from '../../../_actions/user_action'
 
-function RegisterPage() {
+function RegisterPage(props) {
 
   const dispatch = useDispatch();
 
-
   const [Email, setEmail] = useState("")
+  const [Name, setName] = useState("")
   const [Password, setPassword] = useState("")
+  const [ConfirmPassword, setConfirmPassword] = useState("")
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value)
+  }
+
+  const onNameHandler = (event) => {
+    setName(event.currentTarget.value)
   }
 
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value)
   }
 
+  const onConfirmPasswordHandler = (event) => {
+    setConfirmPassword(event.currentTarget.value)
+  }
+
   const onSubmitHandler =(event) => {
-    event.preventDefault(); // 이게 없으면 refresh 
+      event.preventDefault(); // 이게 없으면 refresh 
 
-    //console.log('Email', Email)
-    //console.log('Password', Password)
+      if(Password !== ConfirmPassword){
+        return alert('비밀번호와 비밀번호 확인은 같아야 합니다.')
+      }
 
-    let body = {
-      email: Email,
-      password: Password
-    }
+      let body = {
+        email: Email,
+        password: Password,
+        name:Name
+      }
 
-    // 어떤식으로 loginUser를 부르는지...
-    dispatch(loginUser(body))
+      //redux가 없었으면 
+      //Axios.post('/api/users/register',body)
 
-    Axios.post('/api/user/login', body)
-      .then(response => {
-        if(response.payload.loginSuccess){
-          props.history.push('/')
-        }else{
-          alert('Error')
-        }
-      })
+      // 어떤식으로 loginUser를 부르는지...
+      dispatch(registerUser(body))
+        .then(response => {
+            if(response.payload.success){
+              props.history.push("/login")
+            }else{
+              alert("Failed to sign up")
+            }
+        })
+  }
 
   return (
     <div style={{
@@ -52,15 +64,22 @@ function RegisterPage() {
         >
           <label>Email</label>
           <input type="email" value={Email} onChange={onEmailHandler}/>
+
+          <label>Name</label>
+          <input type="text" value={Name} onChange={onNameHandler}/>
+
           <label>Password</label>
-          <input type="password" value={Password} onChange={onPasswordHandler}/>
+          <input type="Password" value={Password} onChange={onPasswordHandler}/>
+
+          <label>ConfirmPassword</label>
+          <input type="Password" value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
           <br />
           <button type="submit">
-            Login
+            회원가입
           </button>
       </form>
     </div>
   )
 }
 
-export default RegisterPage;
+export default RegisterPage

5285f798f82864a103b6d10c897dacde6c555cba
parkjunyoung <hpaaycim1@naver.com>
2022-07-30 10:35:59 +0900

    component

--- React-Book/react-book/src/App.js
+++ React-Book/react-book/src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import Counter from './Counter';
 import Say from './say';
 import EventPractice from './EventPractice';
+import IterationSample from './IterationSample';
 
 function App() {
   // return <MyComponent name="박준영"/>;
-  return <EventPractice/>;
+  return <IterationSample/>;
 };
 
 export default App;

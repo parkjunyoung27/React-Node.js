4ac7705fc95d5f9db4d0cb9bcc7b9a7d63c08a72
parkjunyoung <hpaaycim1@naver.com>
2022-08-01 01:06:39 +0900

    Error

--- React-Book/react-book/src/App.js
+++ React-Book/react-book/src/App.js
@@ -6,9 +6,10 @@ import { Component } from 'react';
 //import IterationSample from './IterationSample';
 //import ValidationSample from './ValidationSample';
 //import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
-import IterationSample from './IterationSample';
+//import ScrollBox from './ScrollBox';
+//import IterationSample from './IterationSample';
 import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 
 function getRandomColor(){
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -17,19 +18,21 @@ function getRandomColor(){
 class App extends Component{
   state = {
     color : '#000000'
-  }
+  };
 
   handleClick = () => {
     this.setState({
       color: getRandomColor()
     });
-  }
+  };
 
   render(){
     return(
       <div>
         <button onClick={this.handleClick}>랜덤 색상</button>
-        <LifeCycleSample color={this.state.color}/>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }

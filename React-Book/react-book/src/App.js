1368279e7751643573dd0a940ef1af8bde722440
parkjunyoung <hpaaycim1@naver.com>
2022-07-31 20:24:37 +0900

    ref:Dom2

--- React-Book/react-book/src/App.js
+++ React-Book/react-book/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import Counter from './Counter';
-import Say from './say';
-import EventPractice from './EventPractice';
-import IterationSample from './IterationSample';
-import ValidationSample from './ValidationSample';
 import { Component } from 'react';
+//import Counter from './Counter';
+//import Say from './say';
+//import EventPractice from './EventPractice';
+//import IterationSample from './IterationSample';
+//import ValidationSample from './ValidationSample';
+//import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
 
 class App extends Component{
   render(){
     return(
-      <ValidationSample/>
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+        {/* <ValidationSample/> */}
+      </div>
     );
   }
 }

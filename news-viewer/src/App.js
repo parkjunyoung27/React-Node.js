2d31e5e6ec3d4fed84abe51ea9609c4a79f9cd42
parkjunyoung <hpaaycim1@naver.com>
2022-08-17 23:48:41 +0900

    newviewer-end

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,18 +1,13 @@
-import { useState, useCallback } from 'react';
-import axios from 'axios';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import { Route, Routes } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all'); // 기존에 useState로 관리 
-  const onSelect = useCallback(category => setCategory(category), []);
-
-  return(
-      <>
-        <Categories category={category} onSelect={onSelect}/>
-        <NewsList category={category}/>
-      </>
-    ); 
+  return (
+    <Routes>
+      <Route path="/" element={<NewsPage/>}/>
+      <Route path="/:category" element={<NewsPage/>}/>
+    </Routes>
+  );
 };
 
 export default App;
--- news-viewer/src/components/Categories.js
+++ news-viewer/src/components/Categories.js
@@ -1,4 +1,5 @@
-import styled, { css } from 'styled-components'
+import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 const categories = [
     {
@@ -43,7 +44,7 @@ const CategoriesBlock = styled.div`
     }
 `;
 
-const Category = styled.div`
+const Category = styled(NavLink)`
     font-size: 1.125rem;
     cursor: pointer;
     white-space: pre;
@@ -55,29 +56,28 @@ const Category = styled.div`
         color: #495057;
     }
 
-    ${props =>
-        props.active && css`
-            font-weight: 600;
-            border-vottm: 2px solid #22b8cf;
-            color: #22b8cf;
-            &:hover{
-                color: #3bc9db;
-            }
-    `}
+    &.active {
+        font-weight: 600;
+        border-bottom: 2px solid #22b8cf;
+        color: #22b8cf;
+        &:hover{
+            color: #3bc9db;
+        }
+    }
 
     & + &{
         margin-left: 1rem;
     }
 `;
 
-const Categories = ({ onSelect, category }) =>{
+const Categories = () => {
     return(
         <CategoriesBlock>
-            {categories.map(c=>(
-                <Category 
+            {categories.map(c => (
+                <Category
                     key={c.name}
-                    active={category === c.name}
-                    onClick={() => onSelect(c.name)}    
+                    className={({ isActive}) => (isActive ? 'active' : undefined)}
+                    to={c.name === 'all' ? '/' : `\${c.name}`}
                 >
                     {c.text}
                 </Category>

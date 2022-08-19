8a01b692a82bd01d2a48933339682570cab3ec2b
parkjunyoung <hpaaycim1@naver.com>
2022-08-19 23:07:04 +0900

    provide 사용

--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -2,7 +2,7 @@ import {createContext} from 'react';
 
 // 다른 파일과 구분하기 위해 디렉터리를 만듬
 
-const ColorContext = createContext({ color: 'black' });
+const ColorContext = createContext({ color: 'black' }); //안에 consumer가 들어있다 
 
 export default ColorContext;
 

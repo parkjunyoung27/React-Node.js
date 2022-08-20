044433fa57268d826156da2df00972c5d52f6a82
parkjunyoung <hpaaycim1@naver.com>
2022-08-21 02:29:59 +0900

    add redux

--- context-tutorial/src/components/SelectColors.js
+++ context-tutorial/src/components/SelectColors.js
@@ -4,7 +4,9 @@ import ColorContext from '../contexts/color';
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 class SelectColors extends Component{
-    static contextType = ColorContext;
+    static contextType = ColorContext; 
+    // static contextTYpe을 정의하면 클래스 메서드에서도 Contezt에 넣어 둔 함수를 호출할 수 있다는 장점이 있다.
+    // 단점이라면, 한 클래스에서 하나의 Context밖에 사용하지 못한다는 것
 
     handleSetColor = color => {
         this.context.actions.setColor(color);

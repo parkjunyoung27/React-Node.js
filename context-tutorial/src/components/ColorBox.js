a349b288cec5627d4e447cf58ae35b44f6d492b6
parkjunyoung <hpaaycim1@naver.com>
2022-08-20 23:26:02 +0900

    static contextType

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,27 @@
-import { ColorConsumer } from "../contexts/color";
+import { useContext } from "react";
+import ColorContext from "../contexts/color";
 
 
 const ColorBox = () => {
+    const { state } = useContext(ColorContext);
+
     return(
-        <ColorConsumer>
-            {({ state }) => ( //value를 생략할 수도 있음 
-                <>
-                    <div
-                        style={{
-                            width: '64px',
-                            height: '64px',
-                            background: state.color
-                        }}
-                    />
-                    <div
-                        style={{
-                            width: '32px',
-                            height: '32px',
-                            background: state.subcolor
-                        }}        
-                    />
-                </>
-            )}
-        </ColorConsumer>
+        <>
+            <div
+                style={{
+                    width: '64px',
+                    height: '64px',
+                    background: state.color
+                }}
+            />
+            <div
+                style={{
+                    width: '32px',
+                    height: '32px',
+                    background: state.subcolor
+                }}        
+            />
+        </>
     );
 };
 

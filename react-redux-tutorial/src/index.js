b5e7b46044b9827cd55db314ef135fdea0d41b30
parkjunyoung <hpaaycim1@naver.com>
2022-08-30 00:17:41 +0900

    Redux

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import rootReducer from "./modules";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(
+  rootReducer, composeWithDevTools()
+  //,window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+// 컴포넌트에서 store를 사용할 수 있도록 App 컴포넌트를 react-redux에서 제공하는 provider 컴포넌트로 감싸준다.
+
+ReactDom.render(
+  <Provider store={store}> 
+    <App/>
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

f6fd17aafb6eb167afe3dace6ba50d77a6608b60
parkjunyoung <hpaaycim1@naver.com>
2022-09-15 00:26:25 +0900

    미들웨어 구조 만들기

---
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -0,0 +1,5 @@
+const loggerMiddleware = store => next => action => {
+    // 미들웨어 구조
+};
+
+export default loggerMiddleware;
---
+++ learn-redux-middleware/src/modules/counter.js
@@ -0,0 +1,20 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+// 이쪽 부분 중요
+const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
+
+const counter = handleActions(
+    {
+        [INCREASE]: state => state + 1,
+        [DECREASE]: state => state - 1 
+    },
+    initialState
+);
+
+export default counter;

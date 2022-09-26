c9401164832a0b639d7c3199764b4619d4b396c8
parkjunyoung <hpaaycim1@naver.com>
2022-09-26 21:05:04 +0900

    koa 미들웨어

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -2,10 +2,23 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use((ctx, next) => {
+    console.log(ctx.url);
+    console.log(1);
+    next();
+});
+
+app.use((ctx, next) => {
+    console.log(ctx.url);
+    console.log(2);
+    next();
+});
+
 app.use(ctx => {
     ctx.body = 'hello world';
 });
 
+// 포트 4000번으로 오픈 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
 });

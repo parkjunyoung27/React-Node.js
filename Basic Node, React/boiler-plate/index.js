f1c9f0b44f176e8bd3226a68e0a01b12e05f61c5
parkjunyoung <hpaaycim1@naver.com>
2023-03-01 23:45:03 +0900

    비밀번호 암호화

--- Basic Node, React/boiler-plate/index.js
+++ Basic Node, React/boiler-plate/index.js
@@ -22,7 +22,10 @@ app.get('/', (req, res) => { res.send('Hello World! ~ 자동재시작!') })
 // 회원가입 위한 Route 만들기 
 app.post('/register', (req, res) =>{ 
   // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+
   // 그것들을 데이터 베이스에 넣어준다.
+
+  
   const user = new User(req.body)
 
   user.save((err, userInfo) => {

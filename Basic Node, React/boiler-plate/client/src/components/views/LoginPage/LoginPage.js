c32a8004913b5d1d481d345eddc680f238fbcb3f
parkjunyoung <hpaaycim1@naver.com>
2023-03-31 00:17:34 +0900

    로그인 시도

--- Basic Node, React/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ Basic Node, React/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import Axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
 
    const [Email, setEmail ] = useState("")
    const [Password, setPassword ] = useState("")
@@ -13,9 +17,27 @@ function LoginPage() {
     setPassword(event.currentTarget.value)
  }
 
- const onSubmitHandler = (event) => {
-  event.onPasswordHandler();
-}
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        console.log('Email', Email)
+        console.log('Password', Password)
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+          .then(response => {
+              if(response.payload.loginSuccess){
+                props.history.push('/')
+              }else{
+                alert('Error')
+              }
+          })
+
+    }
 
   return (
     <div style={{

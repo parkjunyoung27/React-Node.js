043c58388a1c82698711f1c4a87043ffcdec56ac
parkjunyoung <hpaaycim1@naver.com>
2023-03-30 23:33:52 +0900

    로그인

--- Basic Node, React/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ Basic Node, React/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -12,16 +12,17 @@ function LoginPage() {
    const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value)
  }
-//  const onPasswordHandler = (event) => {
-//   setPassword(event.currentTarget.value)
-// }
+
+ const onSubmitHandler = (event) => {
+  event.onPasswordHandler();
+}
 
   return (
     <div style={{
       display:'flex', justifyContent: 'center', alignItems:'center',
       width:'100%', height:'100vh'
     }}>
-        <form style={{display:'flex', flexDirection:'column'}} 
+        <form style={{display:'flex', flexDirection:'column'}} onSubmit={onSubmitHandler}
           >
           <label>Email</label>
           <input type="email" value={Email} onChange={onEmailHandler}/>

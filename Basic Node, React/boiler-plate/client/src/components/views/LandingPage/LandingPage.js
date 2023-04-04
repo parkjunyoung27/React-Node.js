9f0265cf3386b5106da57e8c95b91beddc7ca5fb
parkjunyoung <hpaaycim1@naver.com>
2023-04-05 00:07:46 +0900

    로그아웃

--- Basic Node, React/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ Basic Node, React/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,23 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
   useEffect(() => {
     axios.get('/api/hello')
     .then(response => console.log(response.data))
   }, [])
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success){
+          props.history.push("/login")
+        } else{
+          alert('로그아웃 하는데 실패 했습니다.')
+        }
+      })
+  }
 
   return (
     <div style={{

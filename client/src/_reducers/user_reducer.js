324d338fbc008a82c7577b258bd5749d6f99e047
parkjunyoung <hpaaycim1@naver.com>
2023-11-24 00:33:40 +0900

    회원가입 완료

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,12 +1,15 @@
-import { LOGIN_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
 
 export default function (state = {}, action){
     switch (action.type) {
         case LOGIN_USER:
-            return { ...state, loginSuccess: action.payload}
+            return { ...state, loginSuccess: action.payload }
             // 로그인 응답을 action.payload에서 받음
             break;
-    
+        case REGISTER_USER:
+            return { ...state, register: action.payload }
+            // 로그인 응답을 action.payload에서 받음
+            break;
         default:
             return state;
     }
